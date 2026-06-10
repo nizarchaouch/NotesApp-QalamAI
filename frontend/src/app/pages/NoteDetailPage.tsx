0a0daaffa3ebfa33c94ad61b42bb4aeacb2fec5a
nizarchaouch <chaouchnizar1@gmail.com>
2026-06-10 22:43:45 +0100

    fix: add onChange handlers for title and content inputs in NoteDetailPage

--- frontend/src/app/pages/NoteDetailPage.tsx
+++ frontend/src/app/pages/NoteDetailPage.tsx
@@ -10,14 +10,22 @@ import useNotesAPI from "@/hooks/useNotesAPI";
 
 export default function NoteDetailPage() {
 
-  const { getNoteById } = useNotesAPI();
   const [note, setNote] = useState<Note | null>(null);
+  const { getNoteById } = useNotesAPI();
   const navigate = useNavigate();
   const { id } = useParams();
 
   const handleBackClick = () => {
     navigate(-1); // Go back to the previous page
   };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNote(prev => prev ? { ...prev, title: e.target.value } : null);
+  }
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNote(prev => prev ? { ...prev, content: e.target.value } : null);
+  }
 
   useEffect(() => {
     const fetchNote = async () => {
@@ -43,8 +51,8 @@ export default function NoteDetailPage() {
         <Button variant="outline" className="cursor-pointer text-red-500" > <Trash2 /> Delete</Button>
       </div>
       <div className="flex flex-col">
-        <Input value={note?.title} className="text-xl font-bold dark:bg-transparent dark:border-none focus-visible:ring-0" />
-        <Textarea value={note?.content} placeholder="Note Content" rows={20} className="min-h-[400px] resize-none dark:bg-transparent dark:border-none focus-visible:ring-0" />
+        <Input value={note?.title} onChange={handleTitleChange} className="text-xl font-bold dark:bg-transparent dark:border-none focus-visible:ring-0" />
+        <Textarea value={note?.content} onChange={handleContentChange} placeholder="Note Content" rows={20} className="min-h-[400px] resize-none dark:bg-transparent dark:border-none focus-visible:ring-0" />
       </div>
     </GlassCard>
   );
